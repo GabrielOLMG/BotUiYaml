7a3bf5fedd5e0522173d3f9ee7bc183607c6b1d1
gguimaraes <gabrielolmg@gmail.com>
2026-04-30 19:26:37 +0100

    fix error

--- packages/botui-manager/src/BotUiManager/api/models/__init__.py
+++ packages/botui-manager/src/BotUiManager/api/models/__init__.py
@@ -1,11 +1,11 @@
 from .step import StepPayload
 from .job import RunBotRequest, RunBotResponse
 from .action import FindRequest
-from .vision import OCRPayload, OCRResult, TemplateMatchPayload, TemplateMatchResult
+from .vision import OCRPayload, TemplateMatchPayload
 
 __all__ = [
     "StepPayload",
     "RunBotRequest", "RunBotResponse",
     "FindRequest",
-    "OCRPayload", "OCRResult", "TemplateMatchPayload", "TemplateMatchResult"
+    "OCRPayload", "TemplateMatchPayload"
 ]
